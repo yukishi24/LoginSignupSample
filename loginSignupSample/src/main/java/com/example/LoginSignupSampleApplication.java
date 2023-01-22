02d39f4fd6fca27023c332acdd43f6bba0f861c1
yukishi24 <yukishi24github@gmail.com>
2023-01-22 21:50:19 +0900

    add: Spring Boot project files

---
+++ loginSignupSample/src/main/java/com/example/LoginSignupSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSignupSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSignupSampleApplication.class, args);
+	}
+
+}
