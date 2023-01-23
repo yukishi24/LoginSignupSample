d3a43639c3efcdd11554a181f1d756c75710579a
yukishi24 <yukishi24github@gmail.com>
2023-01-23 21:07:51 +0900

    add: getting signup.html logic

--- loginSignupSample/src/main/java/com/example/controller/SignupController.java
+++ loginSignupSample/src/main/java/com/example/controller/SignupController.java
@@ -1,5 +1,26 @@
 package com.example.controller;
 
-public class SignupController {
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.application.service.userApplicationService;
 
+@Controller
+@RequestMapping("/user")
+public class SignupController {
+  @Autowired
+  private userApplicationService userApplicationService;
+
+  @GetMapping("/signup")
+  // ユーザー登録画面の表示
+  public String getSignup(Model model) {
+    //性別を取得
+    Map<String, Integer>genderMap = userApplicationService.getGenderMap();
+    model.addAttribute("genderMap",genderMap);
+    
+    return "user/signup";
+  }
 }
