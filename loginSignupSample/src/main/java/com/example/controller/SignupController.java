f0baced172b477a95151e55f76adec0032b13000
yukishi24 <yukishi24github@gmail.com>
2023-01-31 21:08:53 +0900

    add: language conversion logic

--- loginSignupSample/src/main/java/com/example/controller/SignupController.java
+++ loginSignupSample/src/main/java/com/example/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import java.util.Locale;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,10 +17,9 @@ public class SignupController {
   @Autowired
   private userApplicationService service;
 
-
   @GetMapping("/signup")
-  public String getSignup(Model model) {
-    Map<String, Integer> genderMap = service.getGenderMap();
+  public String getSignup(Model model,Locale locale) {
+    Map<String, Integer> genderMap = service.getGenderMap(locale);
     model.addAttribute("genderMap", genderMap);
     return "user/signup";
   }
